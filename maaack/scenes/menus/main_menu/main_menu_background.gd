adc6c1b2789650566b466a5530b59621081f24f8
phil <philipjonesma@gmail.com>
2024-12-09 07:28:03 -0500

    from other laptop

---
+++ maaack/scenes/menus/main_menu/main_menu_background.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+# the idea is to have one or more slimes jumping along the screen.
+# spawn them, set their nav point as the center of the cam, and move the cam to the right along a parrallax background.
+
+
+# Maybe I'll even go extra and have a big upgraded randomized spelltower
+# endlessly shooting the slimes. Shameless ripoff of Dome Keeper
