98d2652d20566ceacaa05be871eec479ff29e342
phil <philipjonesma@gmail.com>
2024-12-30 16:16:59 -0500

    overworld work

--- overworld/scripts/input_handler.gd
+++ overworld/scripts/input_handler.gd
@@ -3,42 +3,34 @@ extends Node
 
 @export var worldgraph : WorldmapView
 @export var danger_box: ColorRect
-@export var player_sprite: Sprite2D
+@export var player_sprite: Node2D
 
 @onready var inactive_node = preload("res://overworld/level_types/inactive.tres")
 
 var current_node_path: NodePath
 var current_node: int
 
+var player_tween: Tween
+
+var input_disabled: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	# load map state along with current node
+	
 	current_node_path = worldgraph.initial_item.get_path()
 	current_node = worldgraph.initial_node
+	
+	# need to add graph coords
+	var worldgraph_pos: Vector2 = worldgraph.get_screen_position()
+	var global_node_pos: Vector2 = worldgraph.get_node(current_node_path).get_node_position(current_node) + worldgraph_pos
+	
+	player_sprite.global_position = global_node_pos
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_map_node_gui_input(event : InputEvent, path : NodePath, node_in_path : int, resource : WorldmapNodeData):
-	#if event is InputEventMouseMotion:
-		#tooltip_root.global_position = event.global_position
-
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-			
-			pass
-			# load game scene
-			
-			
-			#if skilltree.can_activate(path, node_in_path):
-				#tooltip_root.hide()
-				#skilltree.max_unlock_cost -= skilltree.set_node_state(path, node_in_path, 1)
-				#_skillpoints_changed()
-#
-		#if event.button_index == MOUSE_BUTTON_MIDDLE && event.pressed:
-			#skilltree.get_node(path).remove_node(node_in_path)
-
 
 #func _unhandled_input(event : InputEvent):
 	#if event is InputEventMouseButton:
@@ -60,14 +52,37 @@ func _on_load_pressed():
 	worldgraph.load_state(varr[1])
 
 func move_player(path: NodePath, node_in_path: int, global_node_pos: Vector2):
+	input_disabled = true
 	worldgraph.get_node(path).change_node(current_node, inactive_node) 
 	
 	current_node_path = path
 	current_node = node_in_path
 	
-	player_sprite.global_position = global_node_pos
+	player_tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_SINE)
+	#player_tween.tween_property($Sprite, "modulate", Color.RED, 1)
+	#player_tween.tween_property($Sprite, "scale", Vector2(), 1)
+	#player_tween.tween_callback($Sprite.queue_free)
+	#
+	# Animate the player's movement
+	player_tween.tween_property(player_sprite, "global_position", global_node_pos, 2)
+	player_tween.finished.connect(visit_graph_node)
+
+func visit_graph_node():
+	var node_data = worldgraph.get_node_data(current_node_path, current_node)
+	var scene = node_data.data[0]
+	SceneLoader._loaded_resource = scene
+	#SceneLoader.load_scene(scenepath)
+	#print(scenepath)
+	
+	#var selected_scene = scene1 if randf_range(0, 2) < 1 else scene2
+	SceneLoader.change_scene_to_resource()
+	input_disabled = false
 
 func _on_worldmap_view_node_gui_input(event: InputEvent, path: NodePath, node_in_path: int, resource: WorldmapNodeData) -> void:
+	
+		if input_disabled:
+			return
+	
 		if event is InputEventMouseButton:
 			if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
 				
@@ -80,23 +95,20 @@ func _on_worldmap_view_node_gui_input(event: InputEvent, path: NodePath, node_in
 				if danger_rect.has_point(global_node_pos):
 					print("DANGER")
 				else:
-					var node_data = worldgraph.get_node_data(path, node_in_path)
-					print(node_data.id)
+					pass
 				
 				var nodes_adjacent = worldgraph.get_node(path).connection_exists(current_node, node_in_path)
+				var is_active = worldgraph.get_node_state(path, node_in_path) > 0 
+				var can_activate = worldgraph.can_activate(path, node_in_path)
 				
-				if worldgraph.can_activate(path, node_in_path) and nodes_adjacent:
+				if (can_activate or is_active) and nodes_adjacent:
 					#print(worldgraph.get_node_data(path, node_in_path))
 					worldgraph.set_node_state(path, node_in_path, 1)
 					worldgraph.recalculate_map()
 					
-					move_player(path, node_in_path, global_node_pos)
-
-				elif worldgraph.get_node_state(path, node_in_path) > 0 and nodes_adjacent:
+					# save new map state here
+								#		worldgraph.load_state()
+					
 					move_player(path, node_in_path, global_node_pos)
 					
-			#		worldgraph.load_state()
-					#var selected_scene = scene1 if randf_range(0, 2) < 1 else scene2
-			#
-					#SoundManager.play_sfx("reward_button", 0, -6, 1)
-					#SceneLoader.load_scene(selected_scene)
+					SoundManager.play_sfx("reward_button", 0, -6, 1)
