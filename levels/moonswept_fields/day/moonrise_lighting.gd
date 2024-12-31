1f43d10dab876e3f9d07ebcb090f43aeb089ed07
phil <philipjonesma@gmail.com>
2024-12-30 22:55:40 -0500

    i like to see green in my github stats

--- levels/moonswept_fields/day/moonrise_lighting.gd
+++ levels/moonswept_fields/day/moonrise_lighting.gd
@@ -4,8 +4,8 @@ var night_level = preload("res://levels/moonswept_fields/day/moonrise_level.tscn
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var night_level = night_level.instantiate()
-	add_child(night_level)
+	var night_instance = night_level.instantiate()
+	add_child(night_instance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
